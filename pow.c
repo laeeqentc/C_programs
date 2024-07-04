227df08720714ae7f505e1753f48c6d8a78687e8
Laeeq <laeeqentc@gmail.com>
2024-07-04 18:00:18 +0530

    pow math

---
+++ pow.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+int a,i,j;
+a = pow ( 3, 2 ) ;
+//a = pow ( i,j );
+
+printf ( " 3 to the power 2 is %d\n", a);
+
+printf ("enter a number\n");
+scanf("%d",&i);
+
+//j= pow (i,2);
+
+//printf ("%d to the power of 2 is %d\n",i,j);
+return 0;
+
+}
