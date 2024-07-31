84beb30b297b30474329938713d7f183aec3851f
Laeeq <laeeqentc@gmail.com>
2024-07-31 14:41:50 +0530

    update

--- argc__.c
+++ argc__.c
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
     printf("the program name and executable file is %s\n",argv[0]);
     printf("You have entered %d arguments:\nThey are :-\n", argc);
     for (int i = 1; i < argc; i++) {
-        printf("%s\n", argv[i]);
+        printf("position is %d and content is %s\n", i, argv[i]);
     }
     return 0;
 }
