06db5d1234925c3061e6905442ea9d41e3b93956
Laeeq <laeeqentc@gmail.com>
2024-08-04 11:41:11 +0530

    while loop code

---
+++ while_.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i = 1 ;
+printf("while loop code\n i is %dcounter code \n",i);
+
+while ( i <= 10 )
+{
+printf ( "%d\n", i ) ;
+i=i+1;
+}
+printf("i is %d\n", i);
+    return 0;
+}
