044d91a562f2865b98bbc8247f258f75ca2627b2
Laeeq <laeeqentc@gmail.com>
2024-08-21 18:52:45 +0530

    argc argv code, for loop

--- argc_v1.c
+++ argc_v1.c
@@ -1,14 +1,13 @@
-#include<stdio.h>
-int main(int argc, char*argv[])
+#include <stdio.h>
+int main(int argc, char * argv[])
 {
     int i,j;
-    i=0;j=0;
-    printf("program for argc argv\nEnter input parameters\n");
-    printf("program executable name is %s\n",argv[0]);
-    printf("total parameters are %d\n",argc);
-    for (i=1;i<argc;++i)
+    printf ("code for argc, argv[]\n");
+    printf ("progam name is %s\n",argv[0]);
+    printf ("inputs are total %d\n",argc);
+    for (i=0;i<argc;i++)
     {
-        printf("input parameter at position %d is %s\n",i, argv[i]);
+        printf("input at %d position is %s\n", i, argv[i]);
     }
     return 0;
 }
