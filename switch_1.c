2acbf8ef3b7879c90ca9b20ec2cc74eebdb10af6
Laeeq <laeeqentc@gmail.com>
2024-08-26 18:10:01 +0530

    switch practise

--- switch_1.c
+++ switch_1.c
@@ -6,12 +6,16 @@ switch ( i )
 {
 case 1 :
 printf ( "I am in case 1 \n" ) ;
+break;
 case 2 :
 printf ( "I am in case 2 \n" ) ;
+break;
 case 3 :
 printf ( "I am in case 3 \n" ) ;
+break;
 default :
 printf ( "I am in default \n" ) ;
+break;
 }
 return 0;
 }
