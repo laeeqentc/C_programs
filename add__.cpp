19c63034f800de61634f815d5c4d111c334fc654
Laeeq <laeeqentc@gmail.com>
2024-12-09 19:11:37 +0530

    more cpp code

---
+++ add__.cpp
@@ -0,0 +1,18 @@
+// operating with variables
+#include <iostream>
+using namespace std;
+int main ()
+{
+// declaring variables:
+int a, b;
+int result;
+// process:
+a = 5;
+b = 2;
+a = a + 1;
+result = a - b;
+// print out the result:
+cout << "the result is \n"<< result << "\n program over \n";
+// terminate the program:
+return 0;
+}
---
+++ test_1_.cpp
@@ -0,0 +1,10 @@
+/* my second program in C++
+with more comments */
+#include <iostream>
+using namespace std;
+int main ()
+{
+cout << "Hello World! \n"; // prints HelloWorld!
+cout << "I'm a C++ program\n"; // prints I'm a C++ program
+return 0;
+}
