247db725e83b44f85a4f126b3ba8c13592f2177e
laeeqentc <laeeqentc@gmail.com>
2025-05-11 20:28:58 +0530

    argc argv added

--- argc__.c
+++ argc__.c
@@ -1,15 +1,13 @@
-
-// argc argv working 
 #include <stdio.h>
- 
-// defining main with arguments
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    
-    printf("the program name and executable file is %s\n",argv[0]);
-    printf("You have entered %d arguments:\nThey are :-\n", argc);
-    for (int i = 1; i < argc; i++) {
-        printf("position is %d and content is %s\n", i, argv[i]);
+    printf("argc argv \n");
+    int i,j,k=0;
+    for (size_t i = 0; i < argc; i++)
+    {
+        /* code */
+        printf("argc is %d argv is %s\n",argc,argv[i]);    
     }
     return 0;
+
 }
